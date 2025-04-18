2c256a06f1d3b2335d0334f3ee40d59059c8969c
bastostuliojrrt <bastostuliojrrt@gmail.com>
2025-04-17 21:06:50 -0300

    Feat: creation function submit

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 
 @Component({
   selector: 'app-login',
-  imports: [DefaultLoginLayoutComponent],
+  imports: [
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup<LoginForm>;
+
+  constructor() {
+    // Constructor logic can be added here if needed
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  submit() {
+    console.log(this.loginForm.value);
+  }
 
 }
